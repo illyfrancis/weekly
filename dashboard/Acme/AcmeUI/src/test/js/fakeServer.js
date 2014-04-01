dee4607d7691c3b488bd0ef834430539f981905b
illyfrancis <illy.francis@outlook.com>
2014-04-01 09:45:36 -0400

    introducing directory struct for node test only

--- dashboard/Acme/AcmeUI/src/test/js/fakeServer.js
+++ dashboard/Acme/AcmeUI/src/test/js/fakeServer.js
@@ -1,29 +1,29 @@
 /*global sinon*/
-// var jQuery = require('jquery');
+var jQuery = require('jquery');
 
-// function getTodos(listId, callback) {
-//   jQuery.ajax({
-//     url: '/todo/' + listId + '/items',
-//     success: function (data) {
-//       callback(null, data);
-//     }
-//   });
-// }
+function getTodos(listId, callback) {
+  jQuery.ajax({
+    url: '/todo/' + listId + '/items',
+    success: function (data) {
+      callback(null, data);
+    }
+  });
+}
 
-// var assert = require('assert');
-// var url = require('url');
+var assert = require('assert');
+var url = require('url');
 
-// describe('#browser: Fake test', function () {
-//   describe('with stub', function () {
+describe('#browser: Fake test', function () {
+  describe('with stub', function () {
 
-//     afterEach(function () {
-//       jQuery.ajax.restore();
-//     });    
+    afterEach(function () {
+      jQuery.ajax.restore();
+    });    
 
-//     it('make a GET request for todo items', function () {
-//       sinon.stub(jQuery, 'ajax');
-//       getTodos(42, sinon.spy());
-//       assert.equal(jQuery.ajax.calledWithMatch({url: '/todo/42/items'}), true);
-//     });
-//   });
-// });
+    it('make a GET request for todo items', function () {
+      sinon.stub(jQuery, 'ajax');
+      getTodos(42, sinon.spy());
+      assert.equal(jQuery.ajax.calledWithMatch({url: '/todo/42/items'}), true);
+    });
+  });
+});
